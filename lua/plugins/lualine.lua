6c0719ebfe28cf7cfcc29b5dafd2ba5f2541d9e8
everywhereless <everywhereless@gmail.com>
2026-04-26 06:09:31 +0530

    add lualine.nvim

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      theme = 'tokyonight',
+      globalstatus = true,
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' },
+    },
+  },
+}
