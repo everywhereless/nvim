34b6bf3d86ed6f9cc3e4e6af401113d58eb0a2a1
everywhereless <everywhereless@gmail.com>
2026-05-09 01:28:06 +0530

    add typst-preview.nvim

---
+++ lua/plugins/typst-preview.lua
@@ -0,0 +1,13 @@
+return {
+
+  "chomosuke/typst-preview.nvim",
+  lazy = false,
+  version = '1.*',
+  opts = {
+    debug = false,
+    open_cmd = nil,
+    port = 8000,
+    host = "127.0.0.1",
+  },
+
+}
